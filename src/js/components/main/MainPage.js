a5e109cd4c448028759b99eb25cdc9cb7c5d88bd
sapir <sapir.oded1@gmail.com>
2019-11-03 17:40:38 +0200

    all functionality

--- src/js/components/main/MainPage.js
+++ src/js/components/main/MainPage.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Redirect } from 'react-router-dom'
 import './main.sass';
 import SubmitForm from '../simple_user/submitForm/SubmitForm';
-import { Table, Dropdown, DropdownButton } from 'react-bootstrap';
+import { Table, Dropdown, DropdownButton, Spinner, Toast } from 'react-bootstrap';
 import AssociatesPopup from "../simple_user/associates/AssociatesPopup";
 import {observer} from 'mobx-react'
 import Cookies from 'universal-cookie';
@@ -21,35 +21,38 @@ class MainPage extends Component {
         this.props.store.serverHandler.getGames();
     }
 
+    gameChanged(g) {
+        this.setState({game: g.name});
+        this.props.store.serverHandler.getAdminAllAssignments(g._id);
+    }
+
     createAdminTable() {
-        let games = this.props.store.serverHandler.games ? this.props.store.serverHandler.games.map(g=> g.name) : [];
+        let games = this.props.store.serverHandler.games ? this.props.store.serverHandler.games : [];
 
         return (
             <div>
-                <DropdownButton variant="secondary" title={this.state.game}>
+                <DropdownButton className="dropdown-_center" variant="secondary" title={this.state.game}>
                     {games.map(g => <Dropdown.Item key={g._id}
-                                                   onClick={() => this.setState({game: g})}>{g}</Dropdown.Item>)}
+                                                   onClick={this.gameChanged.bind(this, g)}>{g.name}</Dropdown.Item>)}
                 </DropdownButton>
                 <br/>
                 <Table striped bordered hover>
                     <thead>
                     <tr>
                         <th>User Names</th>
-                        <th>User Emails</th>
+                        {/*<th>User Emails</th>*/}
                         <th>Submission Date</th>
                         <th>Score</th>
-                        <th>Rank</th>
                     </tr>
                     </thead>
                     <tbody>
                     {this.props.store.serverHandler.all_assignment_submissions &&
-                    this.props.store.serverHandler.all_assignment_submissions.filter(s => s.game === this.state.game).map(s => (
+                    this.props.store.serverHandler.all_assignment_submissions.map(s => (
                         <tr>
-                            <td>{s.users.join(', ')}</td>
-                            <td>{s.user_emails.join(', ')}</td>
+                            <td>{s.group_ids.join(',')}</td>
+                            {/*<td>/!*s.user_emails.join(', ')*!/</td>*/}
                             <td>{s.submission_date}</td>
-                            <td>{s.score}</td>
-                            <td>{s.rank}</td>
+                            <td>{s.scores.simpleAvg}</td>
                         </tr>
                     ))}
                     </tbody>
@@ -64,17 +67,19 @@ class MainPage extends Component {
                 <thead>
                 <tr>
                     <th>Game</th>
+                    <th>User Names</th>
                     <th>Submission Date</th>
-                    <th>Rank</th>
+                    <th>Score</th>
                 </tr>
                 </thead>
                 <tbody>
                 {this.props.store.serverHandler.assignment_submissions &&
                 this.props.store.serverHandler.assignment_submissions.map(s => (
                     <tr>
-                        <td>{s.game}</td>
+                        <td>{s.game_name}</td>
+                        <td>{s.group_ids.join(',')}</td>
                         <td>{s.submission_date}</td>
-                        <td>{s.rank}</td>
+                        <td>{s.scores.simpleAvg}</td>
                     </tr>
                 ))}
                 </tbody>
@@ -90,17 +95,26 @@ class MainPage extends Component {
                 return <Redirect to='/login' push={true}/>;
         }
 
-        if(this.props.store.serverHandler.user.email === 'admin' &&
-            this.props.store.serverHandler.all_assignment_submissions === null) {
-            this.props.store.serverHandler.getAdminAllAssignments();
-        }
-        else if(this.props.store.serverHandler.user.email !== 'admin' &&
+        if(this.props.store.serverHandler.user.email !== 'admin' &&
             this.props.store.serverHandler.assignment_submissions === null) {
                     this.props.store.serverHandler.getUserAssignments();
         }
 
         return (
             <div>
+                <div className={this.props.store.siteData.isLoading ? "overlay" : "hidden"}>
+                    <Spinner className="loading_icon" animation="grow" />
+                </div>
+                <div className="toast_div">
+                    <div className="toast_body">
+                        <Toast onClose={() => this.props.store.siteData.resetToast()}
+                               show={this.props.store.siteData.toast.show}
+                               delay={3000} autohide>
+                            <Toast.Body>{this.props.store.siteData.toast.text}</Toast.Body>
+                        </Toast>
+                    </div>
+                </div>
+
                 {this.props.store.siteData.openAssociatesPopup && <AssociatesPopup store = {this.props.store}/>}
                 <div className="welcome_msg">
                     Hi {this.props.store.serverHandler.user.name+"  |  "}
